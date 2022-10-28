4ed3080cecb1e72b187390afe863e164e1b156a1
Ramtin Afshar <rafshar95@gmail.com>
2022-10-28 16:32:44 -0700

    116, linked list, recursion

---
+++ 116_populating_next_right_pointers_in_each_node.cc
@@ -0,0 +1,51 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    void ConnectNext(Node*& first_child_node, Node*& last_child_node, Node*& next_child_node){
+        if(next_child_node == nullptr)
+            return;
+        if(first_child_node == nullptr){
+            first_child_node = next_child_node;
+            last_child_node = next_child_node;
+        }
+        else{
+            last_child_node -> next = next_child_node;
+            last_child_node = last_child_node -> next;
+        }
+    }
+    void ConnectAllLevels(Node* root){
+        Node* first_child_node = nullptr;
+        Node* last_child_node = nullptr;
+        Node* current_node = root;
+        while(current_node != nullptr){
+            ConnectNext(first_child_node, last_child_node, current_node -> left);
+            ConnectNext(first_child_node, last_child_node, current_node -> right);
+            current_node = current_node -> next;
+        }
+        if(first_child_node != nullptr)
+            ConnectAllLevels(first_child_node);
+    }
+    Node* connect(Node* root) {
+        if(root == nullptr)
+            return root;
+        ConnectAllLevels(root);
+        return root;
+    }
+};
